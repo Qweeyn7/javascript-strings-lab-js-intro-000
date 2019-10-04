b781fc7d80b185aa461e3d47f5a4b58e0b92a22e
Ebonee Gilford <moonqweyn7@gmail.com>
2019-10-04 02:45:10 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,4 +4,6 @@ var greeting ="Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest ="" + specialGuest + "!"
+var greetSpecialGuest ="" + specialGuest + "!";
+
+var topic = "space"
